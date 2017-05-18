f22780af813b18c5049ea100bf752b8a232f94a5
wangwenchen <wangwenchen@apluslabs.com>
2017-05-18 19:09:04 +0800

    最新版

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,12 +1,13 @@
 const ServerUrl = {
   q: '172.16.46.53',
-  w: '172.16.39.163'
+  w: '172.16.39.163',
+  pro: 'localhost'
 }
 
 module.exports = {
   localhostPort: 8888,
   difficulty: '100.0',
-  globalUrl: ServerUrl.w,
+  globalUrl: ServerUrl.pro,
   globalPort: '8080',
   time_out: 36000
 }
