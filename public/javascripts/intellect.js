abc27321089eb4b2a5a01162b6eae3ba1b4275f5
God <God@Rains-MacBook-Air.local>
2017-05-24 14:11:06 +0800

    shouye

--- public/javascripts/intellect.js
+++ public/javascripts/intellect.js
@@ -5,4 +5,15 @@ $(function() {
   $('.quality .item').on('mouseout', function() {
     $(this).removeClass('active')
   })
+  var li = $('.equipment').find('ul').find('li');
+  for(var i =0 ;i<li.length;i++){
+  		var width = $(li[i]).width();
+  		$(li[i]).find('.bigB').css('height',width+'px');
+  }
+  $(window).on('resize',function(){
+  		for(var i =0 ;i<li.length;i++){
+  			var width = $(li[i]).width();
+  			$(li[i]).find('.bigB').css('height',width+'px');
+  		}
+  })
 })
